2dd5f1461184544e7ad713f3f08591fc8d0c8a9b
Caio Costa <caiocrrodrigues2004@gmail.com>
2020-11-07 12:06:41 -0300

    Initial commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform playerPos;
+    private float playerStartPos;
+
+    void Start() {
+        playerStartPos = playerPos.position.x;
+    }
+
+    void Update() {
+        transform.position = new Vector3(playerPos.position.x - playerStartPos, transform.position.y, transform.position.z);
+    }
+
+}
---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+
+    private SpriteRenderer playerSpriteRenderer;
+    private CapsuleCollider2D playerCollider;
+
+    public bool died = false;
+
+    void Start() {
+        playerSpriteRenderer = GameObject.FindWithTag("Player").GetComponent<SpriteRenderer>();
+        playerCollider = GameObject.FindWithTag("Player").GetComponent<CapsuleCollider2D>();
+    }
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            other.gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+            died = true;
+        }
+    }
+
+    void Update() {
+        if (died) {
+            Color color = playerSpriteRenderer.color;
+            color.a = Mathf.Lerp(color.a, 0f, Time.deltaTime * 6); 
+
+            playerSpriteRenderer.color = color;
+
+            Invoke("Freeze", 0.5f);
+        }
+    }
+
+    void Freeze() {
+        Time.timeScale = 0f;
+    }
+
+
+}
